2b30733caf2d18a9aa6e0893a8dfef884fc4dd6f
nik01575 <nikhil01575@gmail.com>
2023-01-26 01:52:17 +0530

    index.java modified by name

--- index.java
+++ index.java
@@ -1,5 +1,7 @@
 public class index {
     public static void main(String[]args){
         System.out.println("HEllo");
+        System.out.println("HEllo");
+        System.out.println("HEllo");
     }
 }
